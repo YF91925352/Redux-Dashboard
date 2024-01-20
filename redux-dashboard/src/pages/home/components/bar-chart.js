d8ad411b9b678d237978a85e226a774d467c826e
YF91925352 <zyf91925352@outlook.com>
2024-01-20 13:49:07 +0100

    package echarts

---
+++ redux-dashboard/src/pages/home/components/bar-chart.js
@@ -0,0 +1,33 @@
+//封装柱状图组件
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+
+export const BarChart = ({ title }) => {
+  const chartRef = useRef(null);
+  useEffect(() => {
+    //获取渲染的所有dom节点
+    const chartDom = chartRef.current;
+    //图表初始化生成实例对象
+    const myChart = echarts.init(chartDom);
+    //准备图表参数
+    const option = {
+      title: { text: title },
+      xAxis: {
+        type: "category",
+        data: ["Vue", "React", "Angular"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: "bar",
+        },
+      ],
+    };
+    //使用参数进行图标渲染
+    option && myChart.setOption(option);
+  }, []);
+  return <div ref={chartRef} style={{ width: "500px", height: "400px" }}></div>;
+};
--- redux-dashboard/src/pages/home/index.js
+++ redux-dashboard/src/pages/home/index.js
@@ -1,35 +1,10 @@
-import * as echarts from "echarts";
-import { useEffect, useRef } from "react";
+import { BarChart } from "./components/bar-chart";
 
 export const Home = () => {
-  const chartRef = useRef(null);
-  useEffect(() => {
-    //获取渲染的所有dom节点
-    const chartDom = chartRef.current;
-    //图表初始化生成实例对象
-    const myChart = echarts.init(chartDom);
-    //准备图表参数
-    const option = {
-      xAxis: {
-        type: "category",
-        data: ["Vue", "React", "Angular"],
-      },
-      yAxis: {
-        type: "value",
-      },
-      series: [
-        {
-          data: [10, 40, 70],
-          type: "bar",
-        },
-      ],
-    };
-    //使用参数进行图标渲染
-    option && myChart.setOption(option);
-  }, []);
   return (
     <div>
-      <div ref={chartRef} style={{ width: "500px", height: "400px" }}></div>
+      <BarChart title={"Satisfaction of Framewroks"} />
+      <BarChart title={"Usage of Framewroks"} />
     </div>
   );
 };
