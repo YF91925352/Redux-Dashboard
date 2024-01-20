26ac94f10e8fc6ab4d7137428616f5d37ce6e6b3
YF91925352 <zyf91925352@outlook.com>
2024-01-20 13:37:54 +0100

    create echarts

--- redux-dashboard/src/pages/home/index.js
+++ redux-dashboard/src/pages/home/index.js
@@ -1,3 +1,35 @@
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const chartRef = useRef(null);
+  useEffect(() => {
+    //获取渲染的所有dom节点
+    const chartDom = chartRef.current;
+    //图表初始化生成实例对象
+    const myChart = echarts.init(chartDom);
+    //准备图表参数
+    const option = {
+      xAxis: {
+        type: "category",
+        data: ["Vue", "React", "Angular"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: "bar",
+        },
+      ],
+    };
+    //使用参数进行图标渲染
+    option && myChart.setOption(option);
+  }, []);
+  return (
+    <div>
+      <div ref={chartRef} style={{ width: "500px", height: "400px" }}></div>
+    </div>
+  );
 };
